bbde349669545e5b66ad490af0b0a4f84e6d3ee0
Aziz <ibodov.aziz@yandex.ru>
2023-10-24 16:24:59 +0300

    add unittests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,18 @@
-from utils import arrs
+import unittest
+from utils.arrs import get, my_slice
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+class TestArr(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(get([], 0, "test"), "test")
+
+    def test_slice(self):
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3]),  [1, 2, 3])
+        self.assertEqual(my_slice([]), [])
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+if __name__ == '__main__':
+    unittest.main()
